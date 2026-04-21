a440fd7e141bf3e73d2fe4fc883b6ea54cad9afd
Create Value Global <teaportamosvalor@gmail.com>
2026-04-21 16:33:39 +0200

    fix: typeScript errors in build

--- lib/services/stripe.service.ts
+++ lib/services/stripe.service.ts
@@ -9,7 +9,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2026-03-25.dahlia",
   typescript: true,
 });
 
